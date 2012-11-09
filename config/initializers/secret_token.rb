b6829c0fe574c2b1ea424538d34300e31ee261c6
Chris Johnson <cjohnson@foodlogiq.com>
2012-11-09 13:23:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'de716648677b284a8ee1a3284766883382542f9f8894ef90f4587757c5636271687aa6ed391676fc0675376d94674e772114e8ad2bf6734757a77a9c1a7fa313'
